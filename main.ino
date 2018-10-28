e5042a0799f029e5ca03ca370ec11167952da453
hlscalon <henscalon@hotmail.com>
2018-10-28 17:50:41 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,29 +1,28 @@
-#include "LedControl.h"
+#include <LedControl.h>
+#include <ArduinoSTL.h>
 
 struct Controls {
-  Controls(int chUp, int chDown, int chRight, int chLeft) :
-    _chUp(chUp), _chDown(chDown), _chRight(chRight), _chLeft(chLeft) {}
+  Controls(int chUp, int chDown, int chRight, int chLeft, int chFire) :
+    _chUp(chUp), _chDown(chDown), _chRight(chRight), _chLeft(chLeft), _chFire(chFire) {}
 
   int getUp() { return _chUp; }
   int getDown() { return _chDown; }
   int getRight() { return _chRight; }
   int getLeft() { return _chLeft; }
+  int getFire() { return _chFire; }
 
 private:
-  int _chUp; int _chDown; int _chRight; int _chLeft;
+  int _chUp; int _chDown; int _chRight; int _chLeft; int _chFire;
 };
 
 struct Player {
-  Player(int column, const Controls & controls, int dataPin, int clkPin, int csPin) : 
+  Player(int column, const Controls & controls) : 
     _column(column), _controls(controls),
     _positions(0x18), // 00011000
     _score(0), _lastColumn(-1) {
-    _ledControl = LedControl(dataPin, clkPin, csPin, 1);
-    _ledControl.shutdown(0, false);
-    _ledControl.setIntensity(0, 2);
   } 
 
-  void mover(int ch) {
+  void move(int ch) {
     // eh tudo ao contrario 
     if (ch == _controls.getUp()) {
       if (this->getPosition(7)) return; // 128
@@ -44,25 +43,21 @@ struct Player {
     }
   }
 
-  int getColumn() {
-    return _column;
-  }
-
   int getPosition(int row) {
     return (_positions & (0x1 << row)) > 0;
   }
 
-  void print() {
+  void print(LedControl * led) {
     // limpar coluna
     if (_lastColumn > -1) {
       for (int row = 0; row < 8; row++) {
-        _ledControl.setLed(0, _lastColumn, row, 0);
+        led->setLed(0, _lastColumn, row, 0);
       }
       _lastColumn = -1;
     }
     // posicao atual
     for (int row = 0; row < 8; row++) {
-      _ledControl.setLed(0, _column, row, this->getPosition(row));
+      led->setLed(0, _column, row, this->getPosition(row));
     }
   }
 
@@ -72,23 +67,48 @@ private:
   int _score;
   int _lastColumn;
   Controls _controls;
-  LedControl _ledControl = LedControl(0, 0, 0, 1);
 };
 
-Player p1(0, Controls('w', 's', 'a', 'd'), 4, 6, 5);
-Player p2(7, Controls('8', '5', '4', '6'), 11, 13, 12);
+struct Shot {
+
+private:
+  int _firedBy;
+  int _actualLed;
+};
+
+struct Game {
+
+private:
+  std::vector<Shot> shots;
+};
+
+const int dataPin1 = 4, clkPin1 = 6, csPin1 = 5;
+const int dataPin2 = 11, clkPin2 = 13, csPin2 = 12;
+LedControl leds[2] = {
+  LedControl(dataPin1, clkPin1, csPin1, 1), // left
+  LedControl(dataPin2, clkPin2, csPin2, 1)  // right
+};
+
+Player p1(0, Controls('w', 's', 'a', 'd', ' '));
+Player p2(7, Controls('8', '5', '4', '6', '0'));
+Game game;
 
 void setup() {
   Serial.begin(9600); // configura a serial para 9600
+
+  leds[0].shutdown(0, false);
+  leds[0].setIntensity(0, 2);
+  leds[1].shutdown(0, false);
+  leds[1].setIntensity(0, 2);
 }
 
 void loop() {
-  p1.print(); p2.print();
+  p1.print(&leds[0]); p2.print(&leds[1]);
 
   int byteRec; 
   if (Serial.available() > 0) {
     byteRec = Serial.read();
-    p1.mover(byteRec);
-    p2.mover(byteRec);
+    p1.move(byteRec);
+    p2.move(byteRec);
   }
 }
